1c4fd8c42fd937eaf68cc92a50a348ec412f0686
swapniltripathi7 <swapniltripathi2056@gmail.com>
2020-06-28 08:16:10 +0530

    printing test api in json_test.py

---
+++ json_test.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import pprint
+
+response = requests.get('https://api.github.com/')
+
+if response.status_code == 200:
+	print('Success!')
+elif response.status_code == 400:
+	print('Not Found.')
+
+response.encoding = 'utf-8'
+parsed = response.json()
+
+#pp = pprint.PrettyPrinter(indent=4, width=20, compact=False)
+#pp.pprint(parsed)
+print(json.dumps(parsed, indent=4, sort_keys=True))
+
+#print(response.headers)
